aa87b0d3043f8fa64ea8fb2a5161bee8792cfedf
Meredith Black <mblack1190@gmail.com>
2020-05-15 20:36:50 -0500

    editing formatting

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,16 @@
 function generateMarkdown(data) {
-    return `# ${data.projectName}
-            # ${data.projectDescription}
-            # ${data.tableOfContents}
-            # ${data.installationInput}
-            # ${data.usageInput}
-            # ${data.licenseChoices}
-            # ${data.contributorsInput}
-            # ${data.testingInput}
-            # ${data.questionsInput}
-            ### ${data.githubName}
-            ### ${data.gitHubLink}
-            ### ${data.emailAddress}
+    return `#${data.projectName}
+            #${data.projectDescription}
+            #${data.tableOfContents}
+            #${data.installationInput}
+            #${data.usageInput}
+            #${data.licenseChoices}
+            #${data.contributorsInput}
+            #${data.testingInput}
+            #${data.questionsInput}
+            ###${data.githubName}
+            ###${data.gitHubLink}
+            ###${data.emailAddress}
   `;
 }
 
